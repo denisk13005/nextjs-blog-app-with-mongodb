e8eee9a7e6db6eefbf489078700f0869cdd43ad6
denisk13005 <kirmanndenis@gmail.com>
2022-07-19 08:49:50 +0200

    test deploy vercel

--- pages/index.js
+++ pages/index.js
@@ -33,9 +33,9 @@ export default function Home({ posts }) {
 export async function getServerSideProps(ctx) {
     // get the current environment
     let dev = process.env.NODE_ENV !== 'production';
-
+    let server = dev ? "http://localhost:3000" : "https://nextjs-blog-app-with-mongodb-five.vercel.app/"
     // request posts from api
-    let response = await fetch(`${dev ? process.env.DEV_URL : process.env.PROD_URL}/api/posts`);
+    let response = await fetch(`${server}/api/posts`);
     console.log(response);
     // extract the data
     let data = await response.json();
